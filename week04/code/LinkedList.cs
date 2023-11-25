eaa65c94fb130d4c0ec27c64ff1c12ce6411a466
unknown <czutorlevente@gmail.com>
2023-11-25 16:28:39 -0700

    Problem 5 is done!

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -208,7 +208,12 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public IEnumerable Reverse() {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        var curr = _tail;
+        while (curr is not null)
+        {
+            yield return curr.Data;
+            curr = curr.Prev;
+        }
     }
 
     public override string ToString() {
